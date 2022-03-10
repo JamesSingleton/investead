ed2deb06bca2d9089b2ac05cbf24d02eefc492fa
James Singleton <jamesrsingleton1@gmail.com>
2022-03-10 08:33:30 -0700

    chore(deps): update dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  productionBrowserSourceMaps: true,
+  swcMinify: true,
+  reactStrictMode: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
   images: {
+    formats: ["image/avif", "image/webp"],
     domains: ["images.unsplash.com", "images.ctfassets.net"],
   },
 };
